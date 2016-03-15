c71ee432676ef831e9302a1f7c09348922bad4ce
lichengyin <welefen@gmail.com>
2016-03-15 17:23:25 +0800

    release 4.8.0

--- src/index.js
+++ src/index.js
@@ -506,7 +506,7 @@ let chmod = (p, mode) => {
  */
 let md5 = str => {
   let instance = crypto.createHash('md5');
-  instance.update(str + '');
+  instance.update(str + '', 'utf8');
   return instance.digest('hex');
 };
 
